757c5d9db3b50397e838f9d0e7fcaa5e270cf623
Yan Malinovsky <yan.malinovsky@gmail.com>
2016-11-24 15:28:21 +0200

    Adding Items to Shopping List

--- Shoping-List/ViewController.swift
+++ Shoping-List/ViewController.swift
@@ -8,8 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var items:[String] = [String]()
+    
+    @IBOutlet var inputField: UITextField!
+    
+    @IBAction func addButton(_ sender: UIButton) {
+        let newItem:String = inputField.text!
+        items.append(newItem)
+        inputField.resignFirstResponder()
+        inputField.text = ""
+        tableView.reloadData()
+    }
+    
+    @IBOutlet var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +35,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+      return items.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "items_cell", for: indexPath)
+        
+        cell.textLabel?.text = items[indexPath.row]
+        cell.textLabel?.textColor = UIColor.red
+        
+        return cell; 
+    }
 
 }
 
