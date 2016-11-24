c0d09bca3d59455075a88b5371a1c4eea41206f2
Yan Malinovsky <yan.malinovsky@gmail.com>
2016-11-24 16:13:07 +0200

    Selecting Rows and Adding Checkmarks

--- Shoping-List/ViewController.swift
+++ Shoping-List/ViewController.swift
@@ -16,10 +16,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     @IBAction func addButton(_ sender: UIButton) {
         let newItem:String = inputField.text!
-        items.append(newItem)
-        inputField.resignFirstResponder()
-        inputField.text = ""
-        tableView.reloadData()
+        if newItem != ""{
+            items.append(newItem)
+            inputField.resignFirstResponder()
+            inputField.text = ""
+            tableView.reloadData()
+        }
     }
     
     @IBOutlet var tableView: UITableView!
@@ -35,19 +37,28 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // Dispose of any resources that can be recreated.
     }
 
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
-    {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
       return items.count
     }
     
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
-    {
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "items_cell", for: indexPath)
         
         cell.textLabel?.text = items[indexPath.row]
         cell.textLabel?.textColor = UIColor.red
         
         return cell; 
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedRow = tableView.cellForRow(at: indexPath)!
+        
+        if selectedRow.accessoryType == UITableViewCellAccessoryType.none {
+            selectedRow.accessoryType = UITableViewCellAccessoryType.checkmark
+            selectedRow.tintColor = UIColor.green
+        } else {
+            selectedRow.accessoryType = UITableViewCellAccessoryType.none
+        }
     }
 
 }
